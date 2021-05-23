84ad7889352a90a001e053441e0234a8162fbb78
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-05-23 21:13:05 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+let hamburger = document.querySelector('.toggle');
+let bars = document.querySelector('.fa-bars');
+let times = document.querySelector('.fa-times');
+let menu = document.querySelector('nav');
+let bod = document.querySelector('#header');
+
+hamburger.addEventListener('click', ()=> {
+  bars.classList.toggle('activo');
+  times.classList.toggle('activo');
+  menu.classList.toggle('activo');
+});
