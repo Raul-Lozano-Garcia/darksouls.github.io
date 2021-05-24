1a2c5b7b3f2eaf16955688ff1c8e2b3109f98dcf
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-05-24 19:34:11 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
-let hamburger = document.querySelector('.toggle');
-let bars = document.querySelector('.fa-bars');
-let times = document.querySelector('.fa-times');
-let menu = document.querySelector('nav');
-let bod = document.querySelector('#header');
+window.addEventListener("load", () => { 
+  let hamburger = document.querySelector('.toggle');
+  let bars = document.querySelector('.fa-bars');
+  let times = document.querySelector('.fa-times');
+  let menu = document.querySelector('nav');
+  let bod = document.querySelector('#header');
 
-hamburger.addEventListener('click', ()=> {
-  bars.classList.toggle('activo');
-  times.classList.toggle('activo');
-  menu.classList.toggle('activo');
+  hamburger.addEventListener('click', ()=> {
+    bars.classList.toggle('activo');
+    times.classList.toggle('activo');
+    menu.classList.toggle('activo');
+  })
 });
